c8dfa8f86feac5f245d9f4902a86e3260d893676
Chelaru Adrian <adry_net2007@yahoo.com>
2023-05-13 14:17:31 +0300

    Make sure it copy and run scripts from the current dir

--- Scripts/Addons_Menu.sh
+++ Scripts/Addons_Menu.sh
@@ -5,6 +5,8 @@ export TURQ='\033[0;36m'
 export UYELLOW='\033[4;33m'
 export WHITE='\033[0;37m'
 export PROOT_ROOT=$PREFIX/var/lib/proot-distro/installed-rootfs/ubuntu_box86/root
+export SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
 
 echo -e "${GREEN}Run these actions only if you already have the proot with Box86_64 and Wine86_64 configured.";
 echo -e "${GREEN}You can run this menu anytime using './Addons_Menu.sh' inside Termux.";
@@ -24,21 +26,21 @@ echo -e "Anything else: exit."
 read -p "Please select an option:" opt
 case $opt in 
 	1 ) echo -e "${GREEN}Add the 'box' user.${WHITE}"
-		cp Addons_Proot_Add_user.sh $PROOT_ROOT/Addons_Proot_Add_user.sh
+		cp $SCRIPT_DIR/Addons_Proot_Add_user.sh $PROOT_ROOT/Addons_Proot_Add_user.sh
 		chmod +x $PROOT_ROOT/Addons_Proot_Add_user.sh
 		proot-distro login ubuntu_box86 --user root --shared-tmp --no-sysvipc -- bash -c "./Addons_Proot_Add_user.sh"
 		rm $PROOT_ROOT/Addons_Proot_Add_user.sh
 		exit;;
 	2 ) echo -e "${GREEN}Set up Bash x86 and x64.${WHITE}"
-		cp Addons_Proot_Box86_64_Bash.sh $PROOT_ROOT/Addons_Proot_Box86_64_Bash.sh
+		cp $SCRIPT_DIR/Addons_Proot_Box86_64_Bash.sh $PROOT_ROOT/Addons_Proot_Box86_64_Bash.sh
 		chmod +x $PROOT_ROOT/Addons_Proot_Box86_64_Bash.sh
 		proot-distro login ubuntu_box86 --user root --shared-tmp --no-sysvipc -- bash -c "./Addons_Proot_Box86_64_Bash.sh"
 		rm $PROOT_ROOT/Addons_Proot_Box86_64_Bash.sh
 		exit;;
 	3 ) echo -e "${GREEN}Install Steam...${WHITE}"
-		cp Addons_Proot_Add_user.sh $PROOT_ROOT/Addons_Proot_Add_user.sh
-		cp Addons_Proot_Box86_64_Bash.sh $PROOT_ROOT/Addons_Proot_Box86_64_Bash.sh
-		cp Addons_Proot_Steam.sh $PROOT_ROOT/Addons_Proot_Steam.sh
+		cp $SCRIPT_DIR/Addons_Proot_Add_user.sh $PROOT_ROOT/Addons_Proot_Add_user.sh
+		cp $SCRIPT_DIR/Addons_Proot_Box86_64_Bash.sh $PROOT_ROOT/Addons_Proot_Box86_64_Bash.sh
+		cp $SCRIPT_DIR/Addons_Proot_Steam.sh $PROOT_ROOT/Addons_Proot_Steam.sh
 		chmod +x $PROOT_ROOT/Addons_Proot_Add_user.sh
 		chmod +x $PROOT_ROOT/Addons_Proot_Box86_64_Bash.sh
 		chmod +x $PROOT_ROOT/Addons_Proot_Steam.sh
@@ -54,11 +56,11 @@ case $opt in
 		chmod +x ~/.shortcuts/LaunchSteam_proot
 		exit;;
 	4 ) echo -e "${GREEN}Adding tur-repo and install mesa-zink...${WHITE}"
-	    chmod +x Addons_Termux_Mesa_Zink_VirGL.sh
-		./Addons_Termux_Mesa_Zink_VirGL.sh
+	    chmod +x $SCRIPT_DIR/Addons_Termux_Mesa_Zink_VirGL.sh
+		./$SCRIPT_DIR/Addons_Termux_Mesa_Zink_VirGL.sh
 		exit;;
 	5 ) echo -e "${GREEN}Set up Bash x86 and x64.${WHITE}"
-		cp Addons_Proot_GL4ES.sh $PROOT_ROOT/Addons_Proot_GL4ES.sh
+		cp $SCRIPT_DIR/Addons_Proot_GL4ES.sh $PROOT_ROOT/Addons_Proot_GL4ES.sh
 		chmod +x $PROOT_ROOT/Addons_Proot_GL4ES.sh
 		proot-distro login ubuntu_box86 --user root --shared-tmp --no-sysvipc -- bash -c "./Addons_Proot_GL4ES.sh"
 		rm $PROOT_ROOT/Addons_Proot_GL4ES.sh
